09a417f07fb0e5677600bd66192ad2dc1b3a39d4
wnstlrWkd123 <prkjnsk@naver.com>
2025-05-07 15:31:00 +0900

    feat: 변신 기능 추가
    Space Bar 키 입력 시 변신 기능 추가(최고 점수가 10점 이상일 경우에만)

--- Assets/SpartaMetabus/Scripts/BaseController.cs
+++ Assets/SpartaMetabus/Scripts/BaseController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class BaseController : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer characterRenderer;
+    [SerializeField] protected GameObject knightObject;
+    [SerializeField] protected GameObject pumpkinObject;
+
+    private SpriteRenderer knightRenderer;
+    private SpriteRenderer pumpkinRenderer;
 
     protected Rigidbody2D _rigidbody;
 
@@ -10,20 +14,30 @@ public class BaseController : MonoBehaviour
 
     protected Vector2 lookDirection = Vector2.zero;
 
+    protected bool isKnightActive;
+
     protected virtual void Awake()
     {
+        knightRenderer = knightObject.GetComponent<SpriteRenderer>();
+        pumpkinRenderer = pumpkinObject.GetComponent<SpriteRenderer>();
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     protected virtual void Start()
     {
-
+        isKnightActive = knightObject.activeSelf;
     }
 
     protected virtual void Update()
     {
         HandleAction();
         Rotate(lookDirection);
+        if (Input.GetKeyDown(KeyCode.Space) && PlayerPrefs.GetInt("HighScore") >= 10)
+        {
+            knightObject.SetActive(!isKnightActive);
+            pumpkinObject.SetActive(isKnightActive);
+            isKnightActive = knightObject.activeSelf;
+        }
     }
 
     protected virtual void FixedUpdate()
@@ -38,15 +52,21 @@ public class BaseController : MonoBehaviour
 
     private void Movement(Vector2 direction)
     {
-        // 이동속도 관련
-        _rigidbody.velocity = direction *= 10;
+        if (isKnightActive)
+        {
+            _rigidbody.velocity = direction *= 5;
+        }
+        else
+        {
+            _rigidbody.velocity = direction *= 10;
+        }
     }
 
     private void Rotate(Vector2 direction)
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         bool isLeft = Mathf.Abs(rotZ) > 90f;
-
-        characterRenderer.flipX = isLeft;
+        knightRenderer.flipX = isLeft;
+        pumpkinRenderer.flipX = isLeft;
     }
 }
--- Assets/SpartaMetabus/Scripts/PlayerController.cs
+++ Assets/SpartaMetabus/Scripts/PlayerController.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class PlayerController : BaseController
 {
+    private Animator knightAnimator;
+    private Animator pumpkinAnimator;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        knightAnimator = knightObject.GetComponent<Animator>();
+        pumpkinAnimator = pumpkinObject.GetComponent<Animator>();
+    }
+
     protected override void HandleAction()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -11,6 +21,17 @@ public class PlayerController : BaseController
         if (horizontal != 0f)
         {
             lookDirection = new Vector2(horizontal, 0f);
+        }
+
+        bool isAct = movementDirection != Vector2.zero;
+
+        if (isKnightActive)
+        {
+            knightAnimator.SetBool("IsMove", isAct);
+        }
+        else
+        {
+            pumpkinAnimator.SetBool("IsRun", isAct);
         }
     }
 }
